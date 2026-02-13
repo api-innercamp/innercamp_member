ef6852218e5778509a01ed27195899d55495d65d
api-innercamp <sem@innercamp.com>
2026-02-13 16:57:13 +0530

    port change and fallback

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.get("/", (req, res) => {
   res.send("Server is running");
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
